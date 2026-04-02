5bdfc68f2a1e33213200ce12ec6074d6c66e5bf8
maksymsslobodian <maksym.s.slobodian@ukd.edu.ua>
2026-04-02 23:54:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# початкове меню
+# Початкове меню
 dishes = [
     {"name": "Борщ", "price": 100},
     {"name": "Вареники", "price": 80},
     {"name": "Узвар", "price": 30}
 ]
 
-# показує меню і його оформлення
+# Показує меню і його оформлення
 def show_menu():
     print("\n" + "="*30)
     print("      МЕНЮ РЕСТОРАНУ")
@@ -14,7 +14,7 @@ def show_menu():
         print(f"{index}. {dish['name']:<15} | {dish['price']:>5} грн")
     print("="*30)
 
-# щоб додавати страви
+# Щоб додавати страви
 def add_dish():
     print("\n--- Додавання нової страви ---")
     name = input("Введіть назву: ")
@@ -25,15 +25,33 @@ def add_dish():
     except ValueError:
         print("❌ Помилка! Ціна має бути числом.")
 
+# Редагування страви
+def edit_dish():
+    show_menu()
+    try:
+        index = int(input("\nВведіть номер страви для редагування: ")) - 1
+        if 0 <= index < len(dishes):
+            new_name = input("Введіть нову назву: ")
+            new_price = int(input("Введіть нову ціну: "))
+            dishes[index] = {"name": new_name, "price": new_price}
+            print("✅ Дані оновлено!")
+        else:
+            print("❌ Такої страви немає.")
+    except ValueError:
+        print("❌ Помилка введення. Ціна має бути числом.")
+
+# Підрахунок суми
 def calculate_total():
     total = sum(dish['price'] for dish in dishes)
     print(f"\n💰 Загальна вартість усіх страв: {total} грн")
 
+# Основний цикл програми
 if __name__ == "__main__":
     while True:
         show_menu()
         print("1. Додати страву")
         print("2. Порахувати загальну суму")
+        print("3. Редагувати страву")
         print("0. Вихід")
 
         choice = input("\nОберіть дію: ")
@@ -42,6 +60,8 @@ if __name__ == "__main__":
             add_dish()
         elif choice == "2":
             calculate_total()
+        elif choice == "3":
+            edit_dish()
         elif choice == "0":
             print("Дякуємо, що завітали!")
             break
